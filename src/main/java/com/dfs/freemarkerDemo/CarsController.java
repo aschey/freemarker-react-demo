c10989c18fcc709df26a78bb2bd09b9a54129e0e
Austin Schey <aschey13@gmail.com>
2022-05-25 15:59:08 -0500

    fix null error

--- src/main/java/com/dfs/freemarkerDemo/CarsController.java
+++ src/main/java/com/dfs/freemarkerDemo/CarsController.java
@@ -13,27 +13,27 @@ import com.dfs.freemarkerDemo.model.Car;
 public class CarsController {
     @GetMapping(value = "/")
 	public String init(@ModelAttribute("model") ModelMap model, HttpSession session) {
-        var carListAttr = (ArrayList<Car>)session.getAttribute("carList");
-        var carList = new ArrayList<Car>();
-        if (carListAttr == null) {
+        var carList = (ArrayList<Car>)session.getAttribute("carList");
+        if (carList == null) {
+            carList = new ArrayList<>();
             carList.add(new Car("Honda", "Civic"));
             carList.add(new Car("Toyota", "Camry"));
             carList.add(new Car("Nissan", "Altima"));
             session.setAttribute("carList", carList);
         }
-		model.addAttribute("carList", carListAttr);
+		model.addAttribute("carList", carList);
 		return "index";
 	}
 
     @PostMapping(value = "/add")
     public String addCar(@ModelAttribute("car") Car car,  HttpSession session) {
         if (null != car && null != car.getMake() && null != car.getModel() && !car.getMake().isEmpty() && !car.getModel().isEmpty()) {
-            var carListAttr = (ArrayList<Car>)session.getAttribute("carList");
-            if (carListAttr == null) {
-                carListAttr = new ArrayList<>();
+            var carList = (ArrayList<Car>)session.getAttribute("carList");
+            if (carList == null) {
+                carList = new ArrayList<>();
             }
-            carListAttr.add(car);
-            session.setAttribute("carList", carListAttr);
+            carList.add(car);
+            session.setAttribute("carList", carList);
         }
         return "redirect:/";
     }
